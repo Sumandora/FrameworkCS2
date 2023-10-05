fc1d0c6b0a484f0a0035ef4eac350b0681215a4b
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-10-05 17:35:27 +0200

    Update bcrl commit

--- Buildscripts/BCRL.cmake
+++ Buildscripts/BCRL.cmake
@@ -2,6 +2,6 @@
 FetchContent_Declare(bcrl
         GIT_REPOSITORY https://github.com/Sumandora/bcrl
         GIT_PROGRESS TRUE
-        GIT_TAG c3a997c)
+        GIT_TAG 60390f0)
 FetchContent_MakeAvailable(bcrl)
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE bcrl)
