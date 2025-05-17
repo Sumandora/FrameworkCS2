8b558edab13706dd1e763813d018343b6af6c8f1
johannes <johannesmiesenhardt@gmail.com>
2025-05-17 17:49:26 +0200

    Remove old debug prints

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -26,24 +26,7 @@
 #include "SDK/Math/Vector.hpp"
 #include "GameHook/GameHook.hpp"
 
-
-
-void printFields(SchemaClassInfo* classInfo)
-{
-	printf("Regular fields:\n");
-	for (std::size_t i = 0; i < classInfo->fieldsCount; i++) {
-		FieldData* field = &classInfo->fields[i];
-		printf("%s at %x with type %s\n", field->fieldName, field->offset, field->type->name);
-	}
-
-	printf("Static fields:\n");
-	for (std::size_t i = 0; i < classInfo->staticFieldsCount; i++) {
-		StaticFieldData* field = &classInfo->staticFields[i];
-		printf("%s is type of %s and is located at %p\n", field->fieldName, field->type->name, field->data);
-	}
-}
-
-void initializer()
+static void initializer()
 {
 	printf("Hello, world, again!\n");
 
@@ -52,8 +35,6 @@ void initializer()
 	Interfaces::getInterfaces();
 
 	Memory::Create();
-
-	printFields(CSPlayerPawn::classInfo());
 
 	if (!GraphicsHook::hookSDL()) {
 		printf("Failed to hook SDL\n");
