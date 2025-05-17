c8ff24a9f79736c9a472285b9de8fbc6fb764390
johannes <johannesmiesenhardt@gmail.com>
2025-05-17 17:50:41 +0200

    Fix includes

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,29 +1,12 @@
+#include <cstdio>
 #include <thread>
 
-#include "ExecutableMalloc/MemoryManagerAllocator.hpp"
 #include "Interfaces.hpp"
 
 #include "Memory.hpp"
 
 #include "GraphicsHook/GraphicsHook.hpp"
 
-#include "imgui.h"
-#include "SDK/Schema/SchemaClassInfo.hpp"
-#include "SDK/Schema/SchemaSystem.hpp"
-#include "SDK/Schema/SchemaSystemTypeScope.hpp"
-#include "SDK/Schema/Types/FieldData.hpp"
-#include "SDK/Schema/Types/SchemaType.hpp"
-#include "SDK/Schema/Types/StaticFieldData.hpp"
-
-#include "SDK/Entities/BaseEntity.hpp"
-#include "SDK/Entities/GameEntitySystem.hpp"
-
-#include "SDK/Entities/CSPlayerPawn.hpp"
-#include "SDK/GameClass/CollisionProperty.hpp"
-#include "SDK/GameClass/GameSceneNode.hpp"
-#include "SDK/GameClass/Transform.hpp"
-
-#include "SDK/Math/Vector.hpp"
 #include "GameHook/GameHook.hpp"
 
 static void initializer()
