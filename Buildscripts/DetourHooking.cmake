ed3466a1a890d915328f8efc197cba70fb88b460
johannes <johannesmiesenhardt@gmail.com>
2025-05-25 17:29:23 +0200

    Bump dependencies

--- Buildscripts/DetourHooking.cmake
+++ Buildscripts/DetourHooking.cmake
@@ -2,6 +2,6 @@
 FetchContent_Declare(DetourHooking
         GIT_REPOSITORY https://github.com/Sumandora/DetourHooking
         GIT_PROGRESS TRUE
-        GIT_TAG 2.2.2)
+        GIT_TAG 2.3.1)
 FetchContent_MakeAvailable(DetourHooking)
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE DetourHooking)
