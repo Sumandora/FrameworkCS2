c05b8e1a7a917e1d9f4c85810c41903b980a8123
johannes <johannesmiesenhardt@gmail.com>
2025-05-28 17:32:17 +0200

    Add GenericESP as a dependency

---
+++ Buildscripts/GenericESP.cmake
@@ -0,0 +1,7 @@
+# Add GenericESP to the project
+FetchContent_Declare(GenericESP
+        GIT_REPOSITORY https://github.com/Sumandora/GenericESP
+        GIT_PROGRESS TRUE
+        GIT_TAG no-internal-settings)
+FetchContent_MakeAvailable(GenericESP)
+target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE GenericESP)
