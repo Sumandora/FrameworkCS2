70baedfc5bf8720565b695191cc73aef6de5690d
johannes <johannesmiesenhardt@gmail.com>
2025-05-31 12:18:12 +0200

    Rewrite InterfacedLibrary to detect invalid libraries and also make it safer

--- Source/Interfaces.cpp
+++ Source/Interfaces.cpp
@@ -3,10 +3,14 @@
 #include <cstdint>
 #include <cstring>
 #include <dlfcn.h>
+#include <expected>
 #include <format>
+#include <optional>
 #include <string_view>
 #include <unordered_map>
+#include <utility>
 
+#include "BCRL/SearchConstraints.hpp"
 #include "Memory.hpp"
 #include "SDK/GameClass/EngineToClient.hpp"
 #include "SDK/InterfaceReg.hpp"
@@ -15,43 +19,64 @@
 #include "SignatureScanner/PatternSignature.hpp"
 #include "Utils/Logging.hpp"
 
-struct InterfacedLibrary {
+using namespace Interfaces;
+
+InterfacedLibrary::InterfacedLibrary(std::unordered_map<const char*, void*> interfaces)
+	: interfaces(std::move(interfaces))
+{
+}
+
+std::optional<InterfacedLibrary> InterfacedLibrary::create(const char* path)
+{
+	void* handle = dlmopen(LM_ID_BASE, path, RTLD_NOW | RTLD_NOLOAD | RTLD_LOCAL);
+	if (!handle)
+		return std::nullopt;
+
+	void* create_interface_fn = dlsym(handle, "CreateInterface");
+	if (!create_interface_fn)
+		return std::nullopt;
+
+	std::expected<InterfaceReg**, BCRL::FinalizationError>
+		interface_list = BCRL::pointer(Memory::mem_mgr, create_interface_fn)
+							 .add(1)
+							 .relative_to_absolute()
+							 .filter(BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().also([le_path = path](const decltype(Memory::mem_mgr)::RegionT& r) {
+								 auto path = r.get_path();
+								 if (!path.has_value())
+									 return false;
+								 return path->contains(le_path);
+							 }))
+							 .repeater([](auto& ptr) {
+								 if (ptr.does_match(SignatureScanner::PatternSignature::for_array_of_bytes<"48 8b 1d">())) // mov %%rbx, %0
+									 return false;
+								 ptr.next_instruction();
+								 return true;
+							 })
+							 .add(3)
+							 .relative_to_absolute()
+							 .finalize<InterfaceReg**>();
+
+	if (!interface_list.has_value())
+		return std::nullopt;
+
 	std::unordered_map<const char*, void*> interfaces;
-
-	explicit InterfacedLibrary(const char* path)
-	{
-		void* handle = dlmopen(LM_ID_BASE, path, RTLD_NOW | RTLD_NOLOAD | RTLD_LOCAL);
-		void* create_interface_fn = dlsym(handle, "CreateInterface");
-		auto** interface_list = BCRL::pointer(Memory::mem_mgr, create_interface_fn)
-									.add(1)
-									.relative_to_absolute()
-									.repeater([](auto& ptr) {
-										if (ptr.does_match(SignatureScanner::PatternSignature::for_array_of_bytes<"48 8b 1d">())) // mov %%rbx, %0
-											return false;
-										ptr.next_instruction();
-										return true;
-									})
-									.add(3)
-									.relative_to_absolute()
-									.expect<InterfaceReg**>(std::format("Couldn't find interface list for {}", path));
-
-		for (InterfaceReg* interface = *interface_list; interface; interface = interface->m_pNext) {
-			interfaces[interface->m_pName] = interface->m_CreateFn;
-		}
-
-		dlclose(handle);
+	for (InterfaceReg* interface = *interface_list.value(); interface; interface = interface->m_pNext) {
+		interfaces[interface->m_pName] = interface->m_CreateFn;
 	}
 
-	template <typename T>
-	T* get_interface(const char* name) const
-	{
-		for (const auto& [interfaceName, createFn] : interfaces) {
-			if (std::strncmp(name, interfaceName, std::strlen(interfaceName) - 3 /*Ignore the version*/) == 0)
-				return static_cast<T*>(Interfaces::uncover_create_function(createFn));
-		}
-		return nullptr;
+	dlclose(handle);
+
+	return InterfacedLibrary{ interfaces };
+}
+
+void* InterfacedLibrary::get_interface(const char* name) const
+{
+	for (const auto& [interfaceName, createFn] : interfaces) {
+		if (std::strncmp(name, interfaceName, std::strlen(interfaceName) - 3 /*Ignore the version*/) == 0)
+			return Interfaces::uncover_create_function(createFn);
 	}
-};
+	return nullptr;
+}
 
 void* Interfaces::uncover_create_function(void* create_func)
 {
@@ -74,10 +99,10 @@ void* Interfaces::uncover_create_function(void* create_func)
 
 void Interfaces::get_interfaces()
 {
-	auto client = InterfacedLibrary("libclient.so");
-	auto schemasystem = InterfacedLibrary("libschemasystem.so");
-	auto tier0 = InterfacedLibrary("libtier0.so");
-	auto engine2 = InterfacedLibrary("libengine2.so");
+	auto client = InterfacedLibrary::create("libclient.so").value();
+	auto schemasystem = InterfacedLibrary::create("libschemasystem.so").value();
+	auto tier0 = InterfacedLibrary::create("libtier0.so").value();
+	auto engine2 = InterfacedLibrary::create("libengine2.so").value();
 
 	constexpr static auto INFO = [](std::string_view name, void* ptr) {
 		if (ptr)
--- Source/Interfaces.hpp
+++ Source/Interfaces.hpp
@@ -1,10 +1,32 @@
 #pragma once
 
+#include <concepts>
+#include <optional>
+#include <unordered_map>
+
 struct SchemaSystem;
 struct EngineCvar;
 struct EngineToClient;
 
 namespace Interfaces {
+	class InterfacedLibrary {
+		std::unordered_map<const char*, void*> interfaces;
+
+		explicit InterfacedLibrary(std::unordered_map<const char*, void*> interfaces);
+	public:
+		static std::optional<InterfacedLibrary> create(const char* path);
+
+		void* get_interface(const char* name) const;
+
+		template <typename T>
+		T* get_interface(const char* name) const
+		{
+			return static_cast<T*>(get_interface(name));
+		}
+
+		const std::unordered_map<const char*, void*>& get_interfaces() { return interfaces; }
+	};
+
 	inline void* source2Client;
 	inline SchemaSystem* schemaSystem;
 	inline EngineCvar* engineCvar;
