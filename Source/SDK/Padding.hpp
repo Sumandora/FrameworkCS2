1b9e413e28c53c7b8cd2cde515375ae8e8c86005
johannes <johannesmiesenhardt@gmail.com>
2025-06-01 18:32:06 +0200

    Add [[maybe_unused]] to PADDING

--- Source/SDK/Padding.hpp
+++ Source/SDK/Padding.hpp
@@ -5,4 +5,4 @@
 #define CONCAT(a, b) a##b
 #define PADDING_NAME(line) CONCAT(PAD, line)
 
-#define PADDING(size) std::byte PADDING_NAME(__LINE__)[size]{}
+#define PADDING(size) [[maybe_unused]] std::byte PADDING_NAME(__LINE__)[size]{}
