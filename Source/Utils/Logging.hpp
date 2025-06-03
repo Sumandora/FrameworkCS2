93a733b94b85d9802337fc0007a40d1069c3fe4c
johannes <johannesmiesenhardt@gmail.com>
2025-06-03 23:18:05 +0200

    Add glm::vec3 formatter

--- Source/Utils/Logging.hpp
+++ Source/Utils/Logging.hpp
@@ -1,19 +1,47 @@
 #pragma once
 
+#include "glm/ext/vector_float3.hpp"
 #include <cerrno>
 #include <concepts>
 #include <cstring>
 #include <format>
 #include <iostream>
+#include <iterator>
+#include <sstream>
 #include <string>
 #include <string_view>
 #include <type_traits>
 
-template <typename T> requires (!std::same_as<std::remove_cvref_t<T>, void> && !std::same_as<std::remove_cvref_t<T>, char>)
+// TODO put custom formatters into separate file
+
+template <typename T>
+	requires(!std::same_as<std::remove_cvref_t<T>, void> && !std::same_as<std::remove_cvref_t<T>, char>)
 struct std::formatter<T*> : public std::formatter<void*> {
 	constexpr auto format(T* ptr, std::format_context& ctx) const
 	{
 		return std::formatter<void*>::format((void*)ptr, ctx);
+	}
+};
+
+template <>
+struct std::formatter<glm::vec3> {
+	std::formatter<float> float_formatter;
+
+	constexpr auto parse(std::format_parse_context& ctx)
+	{
+		return float_formatter.parse(ctx);
+	}
+
+	auto format(const glm::vec3& vec, std::format_context& ctx) const
+	{
+		*ctx.out()++ = '[';
+		float_formatter.format(vec.x, ctx);
+		*ctx.out()++ = ',';
+		float_formatter.format(vec.y, ctx);
+		*ctx.out()++ = ',';
+		float_formatter.format(vec.z, ctx);
+		*ctx.out()++ = ']';
+		return ctx.out();
 	}
 };
 
