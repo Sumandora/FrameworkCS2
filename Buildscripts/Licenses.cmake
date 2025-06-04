34ab1eaeb3f59a9d94ab245372e73085ff277bcc
johannes <johannesmiesenhardt@gmail.com>
2025-06-04 13:50:49 +0200

    Prevent make from rebuilding the licenses every single time

--- Buildscripts/Licenses.cmake
+++ Buildscripts/Licenses.cmake
@@ -1,13 +1,22 @@
+# This function only writes to a file if the contents differs,
+# this is useful because it avoids updating the modification date and thus doesn't trigger a rebuild.
+function(write_file_if_changed file content)
+	file(READ "${file}" CURRENT_CONTENT)
+	string(COMPARE NOTEQUAL "${content}" "${CURRENT_CONTENT}" NEEDS_UPDATE)
+	if(NEEDS_UPDATE)
+		file(WRITE "${file}" "${content}")
+	endif()
+endfunction()
+
 function(embed_license pretty_name name file)
 	file(READ ${file} license_text)
-	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${name}_license.txt" "R\"(${license_text})\"")
-	set(CONTENT "
+	write_file_if_changed("${CMAKE_CURRENT_BINARY_DIR}/${name}_license.txt" "R\"(${license_text})\"")
+	write_file_if_changed("${CMAKE_CURRENT_BINARY_DIR}/${name}_license.cpp" "
 namespace GUI::Tabs::About {
 	extern const char ${name}_license[] = {
 	#include \"${CMAKE_CURRENT_BINARY_DIR}/${name}_license.txt\"
 	};
 }")
-	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${name}_license.cpp" "${CONTENT}")
 	target_sources(${CMAKE_PROJECT_NAME} PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/${name}_license.cpp")
 	set(LICENSES "${LICENSES} LICENSE(\"${pretty_name}\", ${name})" PARENT_SCOPE)
 endfunction()
