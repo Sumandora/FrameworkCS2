0e735667ec81d21830a450bb640a9fa162f9b20d
johannes <johannesmiesenhardt@gmail.com>
2025-06-05 17:51:29 +0200

    Add ImNodes as a dependency

---
+++ Buildscripts/ImNodes.cmake
@@ -0,0 +1,14 @@
+# Add ImNodes to the project
+FetchContent_Declare(imnodes
+	GIT_REPOSITORY https://github.com/Nelarius/imnodes
+	GIT_PROGRESS TRUE
+	GIT_TAG v0.5)
+FetchContent_MakeAvailable(imnodes)
+
+add_library(imnodes STATIC "${imnodes_SOURCE_DIR}/imnodes.cpp")
+target_include_directories(imnodes PUBLIC ${imnodes_SOURCE_DIR})
+target_link_libraries(imnodes PUBLIC imgui)
+
+target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE imnodes)
+
+# TODO LICENSE
