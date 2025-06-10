f3d47a776ae0adb6a1c928aa3dcc4288aebc29e2
johannes <johannesmiesenhardt@gmail.com>
2025-06-10 22:14:27 +0200

    Add BoxedTabItem element

---
+++ Source/GUI/Elements/BoxedTabItem.cpp
@@ -0,0 +1,20 @@
+#include "BoxedTabItem.hpp"
+#include "imgui.h"
+
+bool ImGuiExt::BeginBoxedTabItem(const char* label, const ImVec2& size, bool* p_open, ImGuiTabItemFlags flags)
+{
+	if (ImGui::BeginTabItem(label, p_open, flags)) {
+		if (!ImGui::BeginChild(label, size, ImGuiChildFlags_Borders | ImGuiChildFlags_AutoResizeY | ImGuiChildFlags_AlwaysAutoResize)) {
+			ImGui::EndChild();
+			return false;
+		}
+		return true;
+	}
+	return false;
+}
+
+void ImGuiExt::EndBoxedTabItem()
+{
+	ImGui::EndChild();
+	ImGui::EndTabItem();
+}
