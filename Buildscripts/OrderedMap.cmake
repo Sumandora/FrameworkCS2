4df146f259f2d48895bafd8b01f373db0ac62dc2
johannes <johannesmiesenhardt@gmail.com>
2025-06-12 21:23:49 +0200

    Add ordered_map as a dependency

---
+++ Buildscripts/OrderedMap.cmake
@@ -0,0 +1,12 @@
+# Add ordered_map to the project
+set(CMAKE_POLICY_VERSION_MINIMUM 3.5 CACHE STRING "" FORCE)
+FetchContent_Declare(ordered_map
+	GIT_REPOSITORY https://github.com/Tessil/ordered-map.git
+	GIT_PROGRESS TRUE
+	GIT_TAG v1.1.0
+	CMAKE_ARGS -DCMAKE_SKIP_INSTALL_RULES=TRUE)
+FetchContent_MakeAvailable(ordered_map)
+
+target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ordered_map)
+
+embed_license("ordered-map" ordered_map "${ordered_map_SOURCE_DIR}/LICENSE")
