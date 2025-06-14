458b7b65b0a76f359ada20cde9e4f6d11afa9d8b
johannes <johannesmiesenhardt@gmail.com>
2025-06-15 00:30:48 +0200

    Add pin pulled and fix class name of BaseCSGrenade

--- Source/SDK/Entities/BaseCSGrenade.hpp
+++ Source/SDK/Entities/BaseCSGrenade.hpp
@@ -7,7 +7,9 @@
 #include "../VirtualMethod.hpp"
 
 struct BaseCSGrenade : BasePlayerWeapon {
-	CLASS_INFO("libclient.so", "BaseCSGrenade");
+	CLASS_INFO("libclient.so", "C_BaseCSGrenade");
+
+	SCHEMA_VAR(bool, pin_pulled, "m_bPinPulled")
 
 	VIRTUAL_METHOD(438, get_grenade_type, int, (), (this));
 };
