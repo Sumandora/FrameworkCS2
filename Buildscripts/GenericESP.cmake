61d126d2bccf705db89d234b579c066460b7b2e6
johannes <johannesmiesenhardt@gmail.com>
2025-06-14 16:52:44 +0200

    Pin GenericESP to a specific commit

--- Buildscripts/GenericESP.cmake
+++ Buildscripts/GenericESP.cmake
@@ -3,6 +3,6 @@ set(IMGUI_DEPENDENCY_OVERRIDE imgui)
 FetchContent_Declare(GenericESP
 	GIT_REPOSITORY https://github.com/Sumandora/GenericESP
 	GIT_PROGRESS TRUE
-	GIT_TAG no-internal-settings)
+	GIT_TAG 7095d68215e7b74d993e52f1aa8cee1c6afeaeff)
 FetchContent_MakeAvailable(GenericESP)
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE GenericESP)
