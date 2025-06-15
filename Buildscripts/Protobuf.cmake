f685ef94578a1eefb622b5895675662995e7fcd1
johannes <johannesmiesenhardt@gmail.com>
2025-06-15 18:30:13 +0200

    Forcefully optimize protobuf to get the size down
    The debug build was 58M big, this is unacceptable. This patch gets it
    down to 4.7M.

--- Buildscripts/Protobuf.cmake
+++ Buildscripts/Protobuf.cmake
@@ -4,21 +4,29 @@ set(protobuf_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 # set(protobuf_WITH_ZLIB OFF CACHE BOOL "" FORCE)
 set(protobuf_DISABLE_RTTI ON CACHE BOOL "" FORCE)
 
+# What a beautiful build system.
+set(ORIG_BUILD_TYPE ${CMAKE_BUILD_TYPE})
+set(CMAKE_BUILD_TYPE Release)
+
 FetchContent_Declare(protobuf
 	GIT_REPOSITORY https://github.com/protocolbuffers/protobuf
 	GIT_PROGRESS TRUE
 	GIT_TAG v3.21.8)
 FetchContent_MakeAvailable(protobuf)
 
-target_compile_options(libprotobuf PRIVATE "-fdata-sections")
+set(CMAKE_BUILD_TYPE ${ORIG_BUILD_TYPE})
+
+target_compile_options(libprotobuf-lite PRIVATE "-fdata-sections" "-Oz")
+target_compile_options(libprotobuf PRIVATE "-fdata-sections" "-Oz")
+target_link_options(libprotobuf-lite PRIVATE "-Wl,--gc-sections")
 target_link_options(libprotobuf PRIVATE "-Wl,--gc-sections")
 
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE libprotobuf)
 
 set(PROTOBUF_DIR "${CMAKE_SOURCE_DIR}/Protobufs")
 file(GLOB_RECURSE PROTOBUF_FILES "${PROTOBUF_DIR}/*.proto")
-set(PROTOBUF_OUTDIR "${CMAKE_CURRENT_BINARY_DIR}/GeneratedProtobufs")
 
+set(PROTOBUF_OUTDIR "${CMAKE_CURRENT_BINARY_DIR}/GeneratedProtobufs")
 file(MAKE_DIRECTORY ${PROTOBUF_OUTDIR})
 
 foreach(FILE ${PROTOBUF_FILES})
@@ -27,6 +35,7 @@ foreach(FILE ${PROTOBUF_FILES})
 	list(APPEND GENERATED_PB_FILES "${GEN_FILE_B}")
 endforeach()
 
+# TODO Can this be prevented from rebuilding every single time?
 add_custom_target(
 	ProtobufGenerate
 	COMMAND "$<TARGET_FILE:protoc>" "--cpp_out" "${PROTOBUF_OUTDIR}" "--proto_path" "${protobuf_SOURCE_DIR}/src/:${PROTOBUF_DIR}" ${PROTOBUF_FILES}
