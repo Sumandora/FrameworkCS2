aae5f95f574846636bbb101080d5006453ebd1d3
johannes <johannesmiesenhardt@gmail.com>
2025-06-15 17:42:22 +0200

    Start using the shiny new protobuf dependency to read the usercmd

--- Source/Hooks/Game/Functions/CreateMove.cpp
+++ Source/Hooks/Game/Functions/CreateMove.cpp
@@ -6,17 +6,20 @@
 
 #include "../../../Utils/Logging.hpp"
 
+#include "glm/ext/vector_float3.hpp"
+
 void* Hooks::Game::CreateMove::hook_func(void* csgo_input, int unk, UserCmd* usercmd)
 {
 	void* ret = RetAddrSpoofer::invoke<void*>(reinterpret_cast<void*>(hook->get_trampoline()), csgo_input, unk, usercmd);
 
-#ifdef DEBUG
 	Logging::info("UserCmd: {}", usercmd);
-	if (UserCmd* cmd = usercmd; cmd)
-		if (BaseCmd* base_cmd = cmd->csgo_usercmd.base_cmd; base_cmd)
-			if (ViewAngles* view_angles = base_cmd->viewangles; view_angles)
-				Logging::info("View angles: {}", view_angles->ang_value);
-#endif
+	if (UserCmd* cmd = usercmd; cmd) {
+		if (usercmd->csgo_usercmd.has_base())
+			if (usercmd->csgo_usercmd.base().has_viewangles()) {
+				const CMsgQAngle& viewangles = usercmd->csgo_usercmd.base().viewangles();
+				Logging::info("View angles: {}", glm::vec3(viewangles.x(), viewangles.y(), viewangles.z()));
+			}
+	}
 
 	return ret;
 }
