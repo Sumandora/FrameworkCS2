bca88dc0562e679fb788aabde6d1eddbe3faaeb6
johannes <johannesmiesenhardt@gmail.com>
2025-06-28 20:02:56 +0200

    Move thread pausing up to the general hooks

--- Source/Hooks/Game/GameHook.cpp
+++ Source/Hooks/Game/GameHook.cpp
@@ -13,16 +13,10 @@
 #include "../../SDK/GameClass/CSGOInput.hpp"
 #include "../../SDK/GameClass/Source2Client.hpp"
 
-#include "../../Utils/Logging.hpp"
-
 #include <csignal>
 #include <cstddef>
-#include <filesystem>
-#include <signal.h>
 #include <string>
-#include <time.h>
 #include <unistd.h>
-#include <vector>
 
 namespace Hooks::Game {
 	void create()
@@ -69,30 +63,12 @@ namespace Hooks::Game {
 				.expect<void*>("Couldn't find radar update function"),
 			reinterpret_cast<void*>(RadarUpdate::hook_func));
 
-		std::vector<pid_t> other_threads;
-
-		const pid_t my_tid = gettid();
-
-		for (const std::filesystem::path& it : std::filesystem::directory_iterator{ "/proc/self/task/" }) {
-			const pid_t p = std::stoi(it.filename());
-			if (p == my_tid)
-				continue;
-
-			kill(p, SIGSTOP);
-			other_threads.push_back(p);
-			Logging::debug("Pausing thread {}", p);
-		}
-
 		FrameStageNotify::hook->enable();
 		ShouldShowCrosshair::hook->enable();
 		FireEvent::hook->enable();
 		GetFunLoading::hook->enable();
 		CreateMove::hook->enable();
 		RadarUpdate::hook->enable();
-
-		for (const pid_t pid : other_threads) {
-			kill(pid, SIGCONT);
-		}
 	}
 
 	void destroy()
--- Source/Hooks/Hooks.cpp
+++ Source/Hooks/Hooks.cpp
@@ -5,8 +5,30 @@
 
 #include "../Utils/Logging.hpp"
 
+#include <csignal>
+#include <filesystem>
+#include <signal.h>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
 void Hooks::create()
 {
+	std::vector<pid_t> other_threads;
+
+	const pid_t my_tid = gettid();
+
+	for (const std::filesystem::path& it : std::filesystem::directory_iterator{ "/proc/self/task/" }) {
+		const pid_t p = std::stoi(it.filename());
+		if (p == my_tid)
+			continue;
+
+		kill(p, SIGSTOP);
+		other_threads.push_back(p);
+	}
+	Logging::debug("Paused threads: {}", other_threads);
+
 	if (!GraphicsHook::hookSDL()) {
 		Logging::error("Failed to hook SDL");
 		return;
@@ -18,6 +40,10 @@ void Hooks::create()
 	}
 
 	Game::create();
+
+	for (const pid_t pid : other_threads) {
+		kill(pid, SIGCONT);
+	}
 }
 
 void Hooks::destroy()
