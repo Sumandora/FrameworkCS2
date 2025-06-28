cf1641cf4cc002082409e7a8fca78daae223cc59
johannes <johannesmiesenhardt@gmail.com>
2025-06-28 17:10:53 +0200

    Add skeleton instance sdk class

---
+++ Source/SDK/GameClass/SkeletonInstance.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../Padding.hpp"
+
+#include "GameSceneNode.hpp"
+
+#include "glm/ext/matrix_float2x4.hpp"
+#include "glm/ext/vector_float3.hpp"
+#include "glm/ext/vector_float4.hpp"
+
+#include <cstdint>
+
+struct SkeletonInstance : GameSceneNode {
+public:
+	PADDING(0x1CC);
+	std::uint32_t bone_count;
+	PADDING(0x20);
+	struct Bone { // I think this is a matrix, but this is more clear.
+		glm::vec3 pos;
+		float scale;
+		glm::vec4 rotation;
+	};
+	static_assert(sizeof(glm::mat2x4) == sizeof(Bone));
+	Bone* bones;
+};
