1ee834f90a2251fde5f78196451b8b823c57c17e
johannes <johannesmiesenhardt@gmail.com>
2025-07-07 14:32:48 +0200

    Add OFFSET macro for defining variables at offsets in a struct

--- Source/SDK/Padding.hpp
+++ Source/SDK/Padding.hpp
@@ -5,4 +5,17 @@
 #define CONCAT(a, b) a##b
 #define PADDING_NAME(line) CONCAT(PAD, line)
 
-#define PADDING(size) [[maybe_unused]] std::byte PADDING_NAME(__LINE__)[size]{}
+#define PADDING(size) \
+	[[maybe_unused]] std::byte PADDING_NAME(__LINE__)[size] { }
+
+#define OFFSET(type, name, offset)                                                             \
+	constexpr auto& name()                                                                     \
+	{                                                                                          \
+		using T = type;                                                                        \
+		return *reinterpret_cast<T*>(reinterpret_cast<std::byte*>(this) + offset);             \
+	}                                                                                          \
+	constexpr auto& name() const                                                               \
+	{                                                                                          \
+		using T = type;                                                                        \
+		return *reinterpret_cast<T const*>(reinterpret_cast<const std::byte*>(this) + offset); \
+	}
