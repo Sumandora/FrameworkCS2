060775d863e2298ea95e788eb538b3201a17f216
johannes <johannesmiesenhardt@gmail.com>
2025-07-12 16:25:31 +0200

    Add LZ4 library

---
+++ Buildscripts/LZ4.cmake
@@ -0,0 +1,13 @@
+# Add lz4 to the project
+FetchContent_Declare(lz4
+	GIT_REPOSITORY https://github.com/lz4/lz4
+	GIT_PROGRESS TRUE
+	GIT_TAG v1.10.0
+	GIT_SHALLOW TRUE)
+FetchContent_MakeAvailable(lz4)
+
+set(LZ4_BUILD_CLI OFF CACHE BOOL "" FORCE)
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
+set(BUILD_STATIC_LIBS ON CACHE BOOL "" FORCE)
+add_subdirectory("${lz4_SOURCE_DIR}/build/cmake/")
+target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE lz4)
