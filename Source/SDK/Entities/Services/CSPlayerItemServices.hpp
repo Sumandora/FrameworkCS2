26d84b17fc89fa49b881eaa61a99478e953af9bf
johannes <johannesmiesenhardt@gmail.com>
2025-07-20 15:14:19 +0200

    Fix retrival of defuser and helmet, by correcting the schema class name of CSPlayerItemServices

--- Source/SDK/Entities/Services/CSPlayerItemServices.hpp
+++ Source/SDK/Entities/Services/CSPlayerItemServices.hpp
@@ -5,7 +5,7 @@
 #include "../../Schema/FieldOffset.hpp"
 
 struct CSPlayerItemServices : public PlayerItemServices {
-	CLASS_INFO("libclient.so", "CPlayer_ItemServices");
+	CLASS_INFO("libclient.so", "CCSPlayer_ItemServices");
 
 	SCHEMA_VAR(bool, has_defuser, "m_bHasDefuser");
 	SCHEMA_VAR(bool, has_helmet, "m_bHasHelmet");
