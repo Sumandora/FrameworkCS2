59e7584d55d9ddc53b4cbe0ff0cf50eb12ebb7e6
johannes <johannesmiesenhardt@gmail.com>
2025-07-23 12:56:46 +0200

    Fix a massive brain issue that I had regarding ImGui Begin/End calls

--- Source/GUI/Elements/BoxedTabItem.cpp
+++ Source/GUI/Elements/BoxedTabItem.cpp
@@ -5,7 +5,7 @@ bool ImGuiExt::BeginBoxedTabItem(const char* label, const ImVec2& size, bool* p_
 {
 	if (ImGui::BeginTabItem(label, p_open, flags)) {
 		if (!ImGui::BeginChild(label, size, ImGuiChildFlags_Borders | ImGuiChildFlags_AutoResizeY | ImGuiChildFlags_AlwaysAutoResize)) {
-			ImGui::EndChild();
+			EndBoxedTabItem();
 			return false;
 		}
 		return true;
