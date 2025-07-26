b9e707c55dd304cf3344108a1e75e3b0a94412e1
johannes <johannesmiesenhardt@gmail.com>
2025-07-26 15:54:59 +0200

    Replace skeleton n-tree with a simple array of draw instructions
    Shadows can sometimes cut off other lines, but that is hard to fix
    outside of GenericESP, so I will need to fix it there.

--- Source/Features/Visuals/ESP/ESP.cpp
+++ Source/Features/Visuals/ESP/ESP.cpp
@@ -1,8 +1,14 @@
 #include "ESP.hpp"
 
+#include <algorithm>
+#include <array>
 #include <concepts>
+#include <cstddef>
 #include <functional>
+#include <initializer_list>
 #include <limits>
+#include <optional>
+#include <ranges>
 #include <string>
 #include <utility>
 #include <vector>
@@ -36,48 +42,40 @@ ESP::ESP()
 {
 }
 
-struct SkeletonTree {
-	int index;
-	std::vector<SkeletonTree> subtrees;
+// TODO Are hardcoded IDs safe here?
+static constexpr std::initializer_list<std::pair<int, int>> SKELETON_LINES{
+	{ 6, 5 },
 
-	template <typename... T>
-		requires(std::same_as<T, SkeletonTree> && ...)
-	explicit SkeletonTree(int index, T... subtress)
-		: index(index)
-		, subtrees{ std::forward<T>(subtress)... }
-	{
-	}
+	// Arms
+	{ 5, 13 },
+	{ 13, 14 },
+	{ 14, 15 },
+
+	{ 5, 8 },
+	{ 8, 9 },
+	{ 9, 10 },
+
+	// Torso
+	{ 5, 4 },
+	{ 4, 3 },
+	{ 3, 2 },
+	{ 2, 1 },
+	{ 1, 0 },
+
+	// Legs
+	{ 0, 25 },
+	{ 25, 26 },
+	{ 26, 27 },
+
+	{ 0, 22 },
+	{ 22, 23 },
+	{ 23, 24 }
 };
-
-// TODO Flatten
-const static SkeletonTree SKELETON_HEAD{
-	6,
-	SkeletonTree{
-		5,
-		// Arms
-		SkeletonTree{ 13, SkeletonTree{ 14, SkeletonTree{ 15 } } },
-		SkeletonTree{ 8, SkeletonTree{ 9, SkeletonTree{ 10 } } },
-
-		SkeletonTree{
-			4,
-			SkeletonTree{
-				3,
-				SkeletonTree{
-					2,
-					SkeletonTree{
-						1,
-						SkeletonTree{
-							0,
-							// Legs
-							SkeletonTree{ 25, SkeletonTree{ 26, SkeletonTree{ 27 } } },
-							SkeletonTree{ 22, SkeletonTree{ 23, SkeletonTree{ 24 } } },
-						},
-					},
-				},
-			},
-		},
-	},
-};
+// TODO This would be prettier with std::ranges::views::concat, but its c++26
+static constexpr std::size_t HIGHEST_SKELETON_INDEX
+	= std::max(
+		std::ranges::max(SKELETON_LINES | std::ranges::views::keys),
+		std::ranges::max(SKELETON_LINES | std::ranges::views::values));
 
 void ESP::draw(ImDrawList* draw_list)
 {
@@ -109,9 +107,11 @@ void ESP::draw(ImDrawList* draw_list)
 			glm::vec3 mins = collision->mins();
 			glm::vec3 maxs = collision->maxs();
 
+			// TODO Vector arithmetic
 			glm::vec3 final_mins = { vec[0] + mins[0], vec[1] + mins[1], vec[2] + mins[2] };
 			glm::vec3 final_maxs = { vec[0] + maxs[0], vec[1] + maxs[1], vec[2] + maxs[2] };
 
+			// TODO This is just cartesian product
 			const glm::vec3 points[] = {
 				// Lower
 				glm::vec3{ final_mins[0], final_mins[1], final_mins[2] },
@@ -159,7 +159,8 @@ void ESP::draw(ImDrawList* draw_list)
 					healthbar.draw(draw_list, entity, unioned_rect);
 			}
 
-			// TODO there can be flickers before the player is respawned, I'm not sure how to approach this quite yet.
+			// TODO The shadows should be drawn first and then have a separate pass adding the actual lines, so that the shadows are actually behind the lines
+			// -> Fix in GenericESP
 			if (this->skeleton.get()) {
 				BodyComponent* body_component = entity->body_component();
 
@@ -168,26 +169,38 @@ void ESP::draw(ImDrawList* draw_list)
 				if (skeleton.bone_count == 0)
 					continue;
 
-				static const std::function<void(const SkeletonInstance&, const SkeletonTree&)> PROCESS_TREE
-					= [&](const SkeletonInstance& skeleton, const SkeletonTree& tree) {
-						  bool can_draw = true;
-						  ImVec2 from_screen_space;
-						  if (!Projection::project(skeleton.bones[tree.index].pos, from_screen_space))
-							  can_draw = false;
+				// The following code mainly tries to avoid projecting points multiple times.
 
-						  for (const SkeletonTree& subtree : tree.subtrees) {
-							  if (can_draw) {
-								  ImVec2 to_screen_space;
-								  if (Projection::project(skeleton.bones[subtree.index].pos, to_screen_space)) {
-									  skeleton_line.draw(draw_list, entity, from_screen_space, to_screen_space);
-								  }
-							  }
+				// Mind that there are gaps in the skeleton lines indexes, so these can't just be computed upfront
+				// So this kind of lazy system is used instead
+				struct ScreenSpacePoint {
+					ImVec2 coords;
+					bool visible : 1 = false;
+					bool looked_up : 1 = false;
 
-							  PROCESS_TREE(skeleton, subtree);
-						  }
-					  };
+					void get(glm::vec3 world_space) &
+					{
+						visible = Projection::project(world_space, coords);
+					}
+				};
+				// Yes, there are holes and yes this is allocating more stack memory than necessary, but it's faster
+				std::array<ScreenSpacePoint, HIGHEST_SKELETON_INDEX + 1 /* 0 is also a valid index */> screen_space_vecs{};
 
-				PROCESS_TREE(skeleton, SKELETON_HEAD);
+				for (const auto& [from, to] : SKELETON_LINES) {
+					ScreenSpacePoint& from_screen_space = screen_space_vecs[from];
+					if (!from_screen_space.looked_up)
+						from_screen_space.get(skeleton.bones[from].pos);
+					if (!from_screen_space.visible)
+						continue;
+
+					ScreenSpacePoint& to_screen_space = screen_space_vecs[to];
+					if (!to_screen_space.looked_up)
+						to_screen_space.get(skeleton.bones[to].pos);
+					if (!to_screen_space.visible)
+						continue;
+
+					skeleton_line.draw(draw_list, entity, from_screen_space.coords, to_screen_space.coords);
+				}
 			}
 		next_ent:;
 		}
