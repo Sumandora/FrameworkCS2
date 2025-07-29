0a11e016fc2a97d16dcf1c2a376857266a1a8516
johannes <johannesmiesenhardt@gmail.com>
2025-07-29 13:36:22 +0200

    Fix LoadKV3 name

--- Source/SDK/GameClass/KeyValues3.cpp
+++ Source/SDK/GameClass/KeyValues3.cpp
@@ -11,7 +11,7 @@ struct UtlString;
 void KeyValues3::load_kv3(const char* text, const KV3ID& kv3_id)
 {
 	static auto fptr = [] {
-		auto* symbol = Libraries::tier0->get_symbol<void* (*)(KeyValues3*, UtlString*, char const*, KV3ID const*, char const*)>("_Z7LoadKV3P10KeyValues3P10CUtlStringPKcRK7KV3ID_tS4_");
+		auto* symbol = Libraries::tier0->get_symbol<void* (*)(KeyValues3*, UtlString*, char const*, KV3ID const*, char const*, unsigned int)>("_Z7LoadKV3P10KeyValues3P10CUtlStringPKcRK7KV3ID_tS4_j");
 
 		if (symbol)
 			Logging::info("Found LoadKV3 at {}", reinterpret_cast<void*>(symbol));
@@ -21,5 +21,5 @@ void KeyValues3::load_kv3(const char* text, const KV3ID& kv3_id)
 		return symbol;
 	}();
 
-	fptr(this, nullptr, text, &kv3_id, "");
+	fptr(this, nullptr, text, &kv3_id, "", 0 /* TODO what is this int, it got added in an update */);
 }
