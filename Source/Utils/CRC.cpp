78b09fd38f6a77f2c8244a888fb3a0a6c47e3077
johannes <johannesmiesenhardt@gmail.com>
2025-07-29 14:29:08 +0200

    Fix CRC computation

--- Source/Utils/CRC.cpp
+++ Source/Utils/CRC.cpp
@@ -11,10 +11,12 @@
 #include "../Memory.hpp"
 
 #include "../SDK/CUtl/Buffer.hpp"
+#include "../SDK/GameClass/MemAlloc.hpp"
 
 #include "google/protobuf/arena.h"
 
 #include <cstddef>
+#include <cstring>
 #include <format>
 #include <vector>
 
@@ -28,58 +30,24 @@
 #include "../SDK/GameClass/UserCmd.hpp"
 
 static int (*create_new_base_cmd)(CBaseUserCmdPB* cmd, int, int) = nullptr;
-static bool (*serialize)(CBaseUserCmdPB* cmd, void* buf, int size) = nullptr;
-static char* (*write_message)(void* begin, void* end) = nullptr; // This is what windows folks call this, I think the name is misleading... it's more like `create_string` or something like that...
-static bool (*set_message_data)(void* move_crc, char** crc_result, google::protobuf::Arena* arena) = nullptr;
-static char** empty_str = nullptr;
-static void* (*operator_delete)(void*, std::size_t) = nullptr;
+static bool (*set_message_data)(void* move_crc, char* crc_result, google::protobuf::Arena* arena) = nullptr;
 
 void CRC::resolve_signatures()
 {
-	// create_new_base_cmd
-	// 	= BCRL::signature(
-	// 		Memory::mem_mgr,
-	// 		SignatureScanner::PatternSignature::for_array_of_bytes<"E8 ? ? ? ? 48 8B 45 ? 83 8D ? ? ? ? 04">(),
-	// 		BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
-	// 		  .add(1)
-	// 		  .relative_to_absolute()
-	// 		  .expect<decltype(create_new_base_cmd)>("Couldn't find create_new_base_cmd");
-	// serialize
-	// 	= BCRL::signature(
-	// 		Memory::mem_mgr,
-	// 		SignatureScanner::PatternSignature::for_array_of_bytes<"41 56 41 55 41 89 D6 41 54 55 49 89 FC">(),
-	// 		BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
-	// 		  .expect<decltype(serialize)>("Couldn't find serialize");
-	// write_message
-	// 	= BCRL::signature(
-	// 		Memory::mem_mgr,
-	// 		SignatureScanner::PatternSignature::for_array_of_bytes<"E8 ? ? ? ? 48 8B 1D ? ? ? ? 48 8B 95">(),
-	// 		BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
-	// 		  .add(1)
-	// 		  .relative_to_absolute()
-	// 		  .expect<decltype(write_message)>("Couldn't find write_message");
-	// set_message_data
-	// 	= BCRL::signature(
-	// 		Memory::mem_mgr,
-	// 		SignatureScanner::PatternSignature::for_array_of_bytes<"41 56 41 55 41 54 55 48 89 FD 53 48 83 EC 10 F6 07 03">(),
-	// 		BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
-	// 		  .expect<decltype(set_message_data)>("Couldn't find set_message_data");
-	// empty_str
-	// 	= BCRL::signature(
-	// 		Memory::mem_mgr,
-	// 		SignatureScanner::PatternSignature::for_array_of_bytes<"48 8B 1D ? ? ? ? 48 8D 43 ? E9 ? ? ? ? 49 8B 46">(),
-	// 		BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
-	// 		  .add(3)
-	// 		  .relative_to_absolute()
-	// 		  .expect<decltype(empty_str)>("Couldn't find empty_str");
-	// operator_delete
-	// 	= BCRL::signature(
-	// 		Memory::mem_mgr,
-	// 		SignatureScanner::PatternSignature::for_array_of_bytes<"E8 ? ? ? ? E9 ? ? ? ? ? ? ? ? ? ? ? 49 8B 56 ? 41 83 4E ? 01">(),
-	// 		BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
-	// 		  .add(1)
-	// 		  .relative_to_absolute()
-	// 		  .expect<decltype(operator_delete)>("Couldn't find operator_delete");
+	create_new_base_cmd
+		= BCRL::signature(
+			Memory::mem_mgr,
+			SignatureScanner::PatternSignature::for_array_of_bytes<"E8 ? ? ? ? 48 8B 45 ? 83 8D ? ? ? ? 04">(),
+			BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
+			  .add(1)
+			  .relative_to_absolute()
+			  .expect<decltype(create_new_base_cmd)>("Couldn't find create_new_base_cmd");
+	set_message_data
+		= BCRL::signature(
+			Memory::mem_mgr,
+			SignatureScanner::PatternSignature::for_array_of_bytes<"41 56 41 55 41 54 55 48 89 FD 53 48 83 EC 10 F6 07 03">(),
+			BCRL::everything(Memory::mem_mgr).thats_readable().thats_executable().with_name("libclient.so"))
+			  .expect<decltype(set_message_data)>("Couldn't find set_message_data");
 }
 
 bool CRC::update_crc(UserCmd* usercmd)
@@ -104,36 +72,31 @@ bool CRC::update_crc(UserCmd* usercmd)
 	UtlBuffer buffer(0, 0, 0);
 	const std::size_t crc_size = new_base_cmd.ByteSizeLong();
 	buffer.ensure_capacity(static_cast<int>(crc_size + 1));
-	const bool success = RetAddrSpoofer::invoke(serialize, &new_base_cmd, static_cast<void*>(buffer.memory.memory), static_cast<int>(crc_size));
+
+	const bool success = new_base_cmd.SerializePartialToArray(buffer.memory.memory, static_cast<int>(crc_size));
 	if (!success) {
 		Logging::error("Failed to generate CRC");
 		return false;
 	}
 
-	bool empty = false;
-	char* local_110 = nullptr;
-	if (crc_size != 0) {
-		local_110 = RetAddrSpoofer::invoke(write_message, static_cast<void*>(buffer.memory.memory), static_cast<void*>(buffer.memory.memory + crc_size));
-	} else {
-		empty = true;
-		local_110 = *empty_str + 0x18;
-	}
+	struct GameString {
+		std::size_t size;
+		std::size_t size2;
+		void* pad2;
+		char data[1]; // VLA
+	};
+
+	auto* puVar20 = static_cast<GameString*>(MemAlloc::the()->allocate(crc_size + 0x19));
+	puVar20->size = crc_size;
+	puVar20->size2 = crc_size;
+	void* puVar26 = &puVar20->data;
+	puVar20->pad2 = nullptr;
+	puVar26 = std::memcpy(puVar26, buffer.memory.memory, crc_size);
 
 	void* crc = &usercmd->csgo_usercmd.mutable_base()->_impl_.move_crc_;
 	usercmd->csgo_usercmd.mutable_base()->_impl_._has_bits_.has_bits_[0] |= 1;
 
-	// Previous CRC does not need to be deallocated because it was allocated in the same arena.
-	RetAddrSpoofer::invoke(set_message_data, crc, &local_110, usercmd->csgo_usercmd.base().GetArena());
-
-	if (!empty) {
-		// Using operator.delete from the game, because I'm not sure about how the whole string ABI stuff works out here...
-		int* var2 = reinterpret_cast<int*>(local_110 + -8);
-		const int var28 = *var2;
-		*var2 = *var2 + -1;
-		if (var28 < 1) {
-			RetAddrSpoofer::invoke(operator_delete, static_cast<void*>(local_110 + -0x18), *reinterpret_cast<std::size_t*>(local_110 + -0x10) + 0x19);
-		}
-	}
+	RetAddrSpoofer::invoke<void>(reinterpret_cast<void*>(set_message_data), crc, static_cast<void*>(&puVar26), usercmd->csgo_usercmd.base().GetArena());
 
 	return true;
 }
