c49beab1ed193d41fd1a5e7bc4c486255de156c6
johannes <johannesmiesenhardt@gmail.com>
2025-07-29 19:13:44 +0200

    Update current_time offset

--- Source/SDK/GameClass/GlobalVars.hpp
+++ Source/SDK/GameClass/GlobalVars.hpp
@@ -5,7 +5,7 @@
 struct GlobalVars {
 	PADDING(0x188);
 
-	OFFSET(float, current_time, 0x34);
+	OFFSET(float, current_time, 0x30);
 };
 
 static_assert(sizeof(GlobalVars) == 0x188); // Probably incorrect...
