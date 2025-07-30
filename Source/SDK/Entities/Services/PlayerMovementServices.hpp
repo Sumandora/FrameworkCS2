ae2e09af35aa882460646da5f496464163c76f98
johannes <johannesmiesenhardt@gmail.com>
2025-07-30 16:45:54 +0200

    Update set and reset prediction_command

--- Source/SDK/Entities/Services/PlayerMovementServices.hpp
+++ Source/SDK/Entities/Services/PlayerMovementServices.hpp
@@ -13,6 +13,6 @@ struct PlayerMovementServices {
 
 	SCHEMA_VAR(glm::vec3, last_movement_impulses, "m_vecLastMovementImpulses");
 
-	VIRTUAL_METHOD(37, set_prediction_command, void, (UserCmd * cmd), (this, cmd));
-	VIRTUAL_METHOD(38, reset_prediction_command, void, (), (this));
+	VIRTUAL_METHOD(38, set_prediction_command, void, (UserCmd * cmd), (this, cmd));
+	VIRTUAL_METHOD(39, reset_prediction_command, void, (), (this));
 };
