ae8b2e187355924c6144fb2394d7681b9686208d
johannes <johannesmiesenhardt@gmail.com>
2025-07-30 16:44:42 +0200

    Fix vote revealer by correcting NetMessagePB::pb offset

--- Source/SDK/NetMessages/NetMessagePB.hpp
+++ Source/SDK/NetMessages/NetMessagePB.hpp
@@ -4,6 +4,6 @@
 
 template <typename PB>
 struct NetMessagePB {
-	PADDING(0x28); // Unsure if this offset is always right, but I looked at a few cases and it seemed to be plausible.
+	PADDING(0x30); // Unsure if this offset is always right, but I looked at a few cases and it seemed to be plausible.
 	PB pb;
 };
