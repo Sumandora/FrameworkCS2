5314f169df30f016802c76269ac3e88e6068e5cc
johannes <johannesmiesenhardt@gmail.com>
2025-08-05 13:14:47 +0200

    Better signatures for all GameParticleManager member functions

--- Source/SDK/Particles/GameParticleManager.cpp
+++ Source/SDK/Particles/GameParticleManager.cpp
@@ -45,28 +45,43 @@ void GameParticleManager::resolve_signatures()
 			  .add(0x18) // This thing has a bunch of XREFs, it's quite easy to find even without looking at other code.
 			  .BCRL_EXPECT(GameParticleManager**, game_particle_manager);
 
+	// Once again, if these signatures go irrecoverably outdated, look at the two particles I mentioned above and look at what they do.
 	::create_particle
 		= BCRL::signature(
 			Memory::mem_mgr,
-			SignatureScanner::PatternSignature::for_array_of_bytes<"E8 ? ? ? ? 41 89 85 ? ? ? ? E8">(),
-			BCRL::everything(Memory::mem_mgr).with_flags("r-x").with_name("libclient.so"))
+			SignatureScanner::PatternSignature::for_literal_string<"particles/entity/spectator_utility_trail.vpcf">(),
+			BCRL::everything(Memory::mem_mgr).with_flags("r--").with_name("libclient.so"))
+			  .find_xrefs(SignatureScanner::XRefTypes::relative(),
+				  BCRL::everything(Memory::mem_mgr).with_flags("r-x").with_name("libclient.so"))
+			  .add(4)
+			  // Dumb initializer function that needs to mention a bunch of random stuff...
+			  .filter([](const auto& ptr) { return ptr.does_match(SignatureScanner::PatternSignature::for_array_of_bytes<"c7">()); })
+			  .repeater([](auto& ptr) {
+				  ptr.next_instruction();
+				  return !ptr.does_match(SignatureScanner::PatternSignature::for_array_of_bytes<"e8">());
+			  })
 			  .add(1)
 			  .relative_to_absolute()
 			  .BCRL_EXPECT(decltype(::create_particle), ::create_particle);
 	::modify_particle
 		= BCRL::signature(
 			Memory::mem_mgr,
-			SignatureScanner::PatternSignature::for_array_of_bytes<"E8 ? ? ? ? E8 ? ? ? ? 66 0F EF C0 49 89 C7">(),
-			BCRL::everything(Memory::mem_mgr).with_flags("r-x").with_name("libclient.so"))
+			SignatureScanner::PatternSignature::for_literal_string<"ANNOTATIONS: hiding node \"%s\"\n">(),
+			BCRL::everything(Memory::mem_mgr).with_flags("r--").with_name("libclient.so"))
+			  .find_xrefs(SignatureScanner::XRefTypes::relative(),
+				  BCRL::everything(Memory::mem_mgr).with_flags("r-x").with_name("libclient.so"))
+			  .prev_signature_occurrence(SignatureScanner::PatternSignature::for_array_of_bytes<"e8 ? ? ? ? 49">())
 			  .add(1)
 			  .relative_to_absolute()
 			  .BCRL_EXPECT(decltype(::modify_particle), ::modify_particle);
 	::attach_particle_to_snapshot
 		= BCRL::signature(
 			Memory::mem_mgr,
-			SignatureScanner::PatternSignature::for_array_of_bytes<"E8 ? ? ? ? E9 ? ? ? ? ? ? ? ? ? ? ? ? ? 41 0F B6 85">(),
-			BCRL::everything(Memory::mem_mgr).with_flags("r-x").with_name("libclient.so"))
-			  .add(1)
+			SignatureScanner::PatternSignature::for_literal_string<"prop_group">(),
+			BCRL::everything(Memory::mem_mgr).with_flags("r--").with_name("libclient.so"))
+			  .find_xrefs(SignatureScanner::XRefTypes::relative(),
+				  BCRL::everything(Memory::mem_mgr).with_flags("r-x").with_name("libclient.so"))
+			  .sub(7)
 			  .relative_to_absolute()
 			  .BCRL_EXPECT(decltype(::attach_particle_to_snapshot), ::attach_particle_to_snapshot);
 }
