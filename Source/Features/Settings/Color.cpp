a7e16db5b71f77a8a2e1b1e8b26d2231bb759efc
johannes <johannesmiesenhardt@gmail.com>
2025-08-14 21:17:24 +0200

    Replace std::atomic<ImColor> with shared_mutex

--- Source/Features/Settings/Color.cpp
+++ Source/Features/Settings/Color.cpp
@@ -4,7 +4,9 @@
 
 #include "imgui.h"
 
-#include <atomic>
+#include <array>
+#include <mutex>
+#include <shared_mutex>
 #include <string>
 #include <utility>
 
@@ -16,7 +18,6 @@ RawColor::RawColor(SettingsHolder* parent, std::string name, ImColor value)
 
 void RawColor::render()
 {
-	ImColor value = this->value.load(std::memory_order::acquire);
 	const bool clicked = ImGui::ColorButton((get_name() + "##Button").c_str(), value, ImGuiColorEditFlags_None, ImVec2(0, 0));
 	ImGui::SameLine();
 	ImGui::Text("%s", get_name().c_str());
@@ -26,10 +27,14 @@ void RawColor::render()
 		ImGui::OpenPopup(id.c_str());
 
 	if (ImGui::BeginPopup(id.c_str())) {
-		float float_array[] = { value.Value.x, value.Value.y, value.Value.z, value.Value.w };
-		if (ImGui::ColorPicker4("##Picker", float_array, 0)) {
+		std::array<float, 4> float_array;
+		{
+			const std::shared_lock lock{ mutex };
+			float_array = { value.Value.x, value.Value.y, value.Value.z, value.Value.w };
+		}
+		if (ImGui::ColorPicker4("##Picker", float_array.data())) {
+			const std::unique_lock lock{ mutex };
 			value.Value = ImVec4(float_array[0], float_array[1], float_array[2], float_array[3]);
-			this->value.store(value);
 		}
 		ImGui::EndPopup();
 	}
@@ -37,17 +42,17 @@ void RawColor::render()
 
 void RawColor::serialize(nlohmann::json& output_json) const
 {
-	ImColor value = this->value.load();
+	const std::unique_lock lock{ mutex };
 	output_json = { value.Value.x, value.Value.y, value.Value.z, value.Value.w };
 }
 
 void RawColor::deserialize(const nlohmann::json& input_json)
 {
-	const ImColor color{
+	const std::unique_lock lock{ mutex };
+	value = {
 		input_json[0].get<float>(),
 		input_json[1].get<float>(),
 		input_json[2].get<float>(),
 		input_json[3].get<float>(),
 	};
-	value.store(color);
 }
--- Source/Features/Settings/Color.hpp
+++ Source/Features/Settings/Color.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
-#include "../Setting.hpp"
 #include "../Instrumentation/InstrumentableSetting.hpp"
+#include "../Setting.hpp"
 
 #include "imgui.h"
 
-#include <atomic>
+#include <shared_mutex>
 #include <string>
 
 class RawColor : public Setting {
-	std::atomic<ImColor> value;
+	ImColor value;
+	mutable std::shared_mutex mutex;
 
 public:
 	RawColor(SettingsHolder* parent, std::string name, ImColor value = { 1.0F, 1.0F, 1.0F, 1.0F });
 
-	[[nodiscard]] ImColor get() const { return value.load(std::memory_order::relaxed); }
+	[[nodiscard]] ImColor get() const
+	{
+		const std::shared_lock lock{ mutex };
+		return value;
+	}
 
 	void render() override;
 	void serialize(nlohmann::json& output_json) const override;
