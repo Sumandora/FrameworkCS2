fc5be7c373916e90ad31b2af4b9d33adb12309d5
johannes <johannesmiesenhardt@gmail.com>
2025-08-14 21:14:36 +0200

    Remove thread pausing during hooking
    This didn't really solve anything and mostly added incompatibilities. I
    think it's not worth fixing 0.01% of issues while creating dozens of
    other problems.

--- Source/Hooks/Hooks.cpp
+++ Source/Hooks/Hooks.cpp
@@ -6,40 +6,11 @@
 #include "../Utils/Logging.hpp"
 
 #include <csignal>
-#include <filesystem>
-#include <signal.h>
-#include <string>
 #include <sys/types.h>
 #include <unistd.h>
-#include <vector>
 
 void Hooks::create()
 {
-	std::vector<pid_t> other_threads;
-
-	const pid_t my_tid = gettid();
-
-	for (const std::filesystem::path& it : std::filesystem::directory_iterator{ "/proc/self/task/" }) {
-		const pid_t p = std::stoi(it.filename());
-		if (p == my_tid)
-			continue;
-
-		kill(p, SIGSTOP);
-		other_threads.push_back(p);
-	}
-#ifdef __cpp_lib_format_ranges
-	Logging::debug("Paused threads: {}", other_threads);
-#else
-	// temporary, bad reimplementation:
-	std::string vec_string;
-	for (const pid_t pid : other_threads) {
-		if (!vec_string.empty())
-			vec_string += ", ";
-		vec_string += std::to_string(pid);
-	}
-	Logging::debug("Paused threads: [{}]", vec_string);
-#endif
-
 	Game::create();
 
 	if (!GraphicsHook::hookSDL()) {
@@ -50,10 +21,6 @@ void Hooks::create()
 	if (!GraphicsHook::hookVulkan()) {
 		Logging::error("Failed to hook vulkan");
 		return;
-	}
-
-	for (const pid_t pid : other_threads) {
-		kill(pid, SIGCONT);
 	}
 }
 
