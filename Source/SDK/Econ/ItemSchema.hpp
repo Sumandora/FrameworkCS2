be3f3e6a70e3a062c5b020e0f3ef698adac7e86b
johannes <johannesmiesenhardt@gmail.com>
2025-08-15 02:49:19 +0200

    Update EconItemSchema offsets

--- Source/SDK/Econ/ItemSchema.hpp
+++ Source/SDK/Econ/ItemSchema.hpp
@@ -10,14 +10,15 @@ struct EconItemDefinition;
 
 struct EconItemSchema {
 private:
-	PADDING(0x100);
+	PADDING(0xf8);
 
 public:
 	struct ItemEntry {
 		int id;
 		PADDING(0x4);
 		EconItemDefinition* econ_item_definition;
-		PADDING(0x8);
+		int unk;
+		PADDING(0x4);
 	};
 	UtlVector<ItemEntry> items;
 };
