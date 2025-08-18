e010424768af8f6000eea9e6076d6ebac81ebd56
johannes <johannesmiesenhardt@gmail.com>
2025-08-18 22:34:20 +0200

    Improve keybind serialization

--- Source/GUI/InputManager.hpp
+++ Source/GUI/InputManager.hpp
@@ -2,9 +2,14 @@
 
 #include "imgui.h"
 
+#include "magic_enum/magic_enum.hpp"
+
+#include <algorithm>
 #include <cstdint>
 #include <memory>
+#include <optional>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace GUI {
@@ -20,7 +25,11 @@ namespace GUI {
 				TOGGLE,
 			};
 
-			std::string name;
+			// Serialization keys
+			static constexpr std::string_view KEY = "Key";
+			static constexpr std::string_view MOUSE_BUTTON = "Mouse button";
+			static constexpr std::string_view METHOD = "Method";
+			static constexpr std::string_view TYPE = "Type";
 			union {
 				ImGuiKey key;
 				ImGuiMouseButton mouse_button;
@@ -32,35 +41,44 @@ namespace GUI {
 			template <typename BasicJsonType>
 			friend void to_json(BasicJsonType& json, const Key& key)
 			{
-				json["Name"] = key.name;
-				json["Method"] = key.method;
-				json["Type"] = key.type;
-				json["Active"] = key.active;
+				// TODO More general, but don't know where to put: Assign string constants to these serialize/deserialize functions to not repeat the keys
+				json[TYPE] = magic_enum::enum_name(key.type);
+
+				if (key.method == Method::KEY && key.key == ImGuiKey_None)
+					// empty key
+					return;
+
 				switch (key.method) {
-				case InputManager::Key::Method::KEY:
-					json["Key"] = key.key;
+				case Method::KEY:
+					json[KEY] = key.key;
 					break;
-				case InputManager::Key::Method::MOUSE:
-					json["Mouse Button"] = key.mouse_button;
+				case Method::MOUSE:
+					json[MOUSE_BUTTON] = key.mouse_button;
 					break;
 				}
 			}
 			template <typename BasicJsonType>
 			friend void from_json(const BasicJsonType& json, Key& key)
 			{
-				json.at("Name").get_to(key.name);
-				key.method = json["Method"];
-				key.type = json["Type"];
-				key.active = json["Active"];
+				if (json.contains(TYPE) && json[TYPE].is_string())
+					if (std::optional<Type> optional_type = magic_enum::enum_cast<Type>(json[TYPE].template get<std::string>()); optional_type.has_value())
+						key.type = optional_type.value();
 
-				switch (key.method) {
-				case InputManager::Key::Method::KEY:
-					json.at("Key").get_to(key.key);
-					break;
-				case InputManager::Key::Method::MOUSE:
-					json.at("Mouse Button").get_to(key.mouse_button);
-					break;
+				if (json.contains(KEY) && json[KEY].is_number()) {
+					key.key = std::clamp(json[KEY].template get<ImGuiKey>(), ImGuiKey_NamedKey_BEGIN, ImGuiKey_NamedKey_END);
+					key.method = Method::KEY;
+					return;
 				}
+
+				if (json.contains(MOUSE_BUTTON) && json[MOUSE_BUTTON].is_number()) {
+					key.mouse_button = std::clamp(json[MOUSE_BUTTON].template get<ImGuiMouseButton>(), 0, ImGuiMouseButton_COUNT - 1);
+					key.method = Method::MOUSE;
+					return;
+				}
+
+				// If we survived until here then it's pointless assign an empty key
+				key.method = Method::KEY;
+				key.key = ImGuiKey_None;
 			};
 
 		public:
